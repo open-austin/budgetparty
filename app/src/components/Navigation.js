66a02fa565a590e8957ed1460359cc6ec5e4277a
Mateo Clarke <code@mateoclarke.com>
2017-03-28 01:58:03 -0500

    Update navigation to design mock

--- app/src/components/Navigation.js
+++ app/src/components/Navigation.js
@@ -1,27 +1,18 @@
 import React, { Component } from 'react'
 import User from './User'
+import TotalFundAvailable from './TotalFundAvailable'
 import { Link } from 'react-router-dom'
 import avatar from '../images/avatar.svg'
 
 export default class Navigation extends Component {
   render () {
     return (
-      <nav className="navbar navbar-default navbar-static-top">
-        <div className="container">
-          <Link to="/user">
-            <img src={avatar} alt="User Account"/>
+      <nav className="Navigation navbar navbar-default navbar-static-top">
+        <div className="container flexbox">
+          <Link to="/user" className="flex">
+            <img src={avatar} alt="User Account" className="Navigation__user-icon"/>
           </Link>
-          <div className="navbar-header">
-            <Link to="/" className="navbar-brand">React Router + Firebase Auth</Link>
-          </div>
-          <ul className="nav navbar-nav pull-right">
-            <li>
-              <Link to="/" className="navbar-brand">Home</Link>
-            </li>
-            <li>
-              <Link to="/dashboard" className="navbar-brand">Dashboard</Link>
-            </li>
-          </ul>
+          <TotalFundAvailable />
         </div>
       </nav>
     )
