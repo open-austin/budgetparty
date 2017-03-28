fc3d2b60627ebb972427d2379ce773b90fba70ca
Mateo Clarke <code@mateoclarke.com>
2017-03-28 01:30:24 -0500

    Create User route + component

--- app/src/components/Navigation.js
+++ app/src/components/Navigation.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
+import User from './User'
 import { Link } from 'react-router-dom'
+import avatar from '../images/avatar.svg'
 
 export default class Navigation extends Component {
   render () {
     return (
       <nav className="navbar navbar-default navbar-static-top">
         <div className="container">
+          <Link to="/user">
+            <img src={avatar} alt="User Account"/>
+          </Link>
           <div className="navbar-header">
             <Link to="/" className="navbar-brand">React Router + Firebase Auth</Link>
           </div>
@@ -15,16 +20,6 @@ export default class Navigation extends Component {
             </li>
             <li>
               <Link to="/dashboard" className="navbar-brand">Dashboard</Link>
-            </li>
-            <li>
-              {this.props.isAuthed
-                ? <button
-                    style={{border: 'none', background: 'transparent'}}
-                    onClick={this.props.handleLogout}
-                    className="navbar-brand">Logout</button>
-                : <span>
-                    <Link to="/login" className="navbar-brand">Login</Link>
-                  </span>}
             </li>
           </ul>
         </div>
--- app/src/components/index.js
+++ app/src/components/index.js
@@ -10,6 +10,7 @@ import Register from './Register'
 import Home from './Home'
 import Intro from './Intro'
 import Dashboard from './Dashboard'
+import User from './User'
 import { firebaseAuth, googleAuthProvider } from '../config/constants'
 import { logout, login, resetPassword, auth } from '../helpers/auth'
 
@@ -86,6 +87,7 @@ export default class App extends Component {
               />
               <Route path='/intro/:id' render={(props) => <Intro {...props} authed={this.state.authed} handleLogout={this.handleLogout.bind(this)} />} />
               <Route path='/dashboard' render={props => <Dashboard isAuthed={this.state.authed} handleLogout={this.handleLogout.bind(this)} />} />
+              <Route path='/user' render={props => <User isAuthed={this.state.authed} handleLogout={this.handleLogout.bind(this)} />} />
               <Route render={() => <h3>404, you ain't supposed to be here</h3>} />
             </Switch>
           </div>
