8eccae85dda0822f6a75346ca9e9381e82394965
gness1804 <graham.nessler@gmail.com>
2017-05-11 14:45:32 -0500

    Do PropTypes for Tabs

--- app/src/components/Tabs.js
+++ app/src/components/Tabs.js
@@ -1,15 +1,17 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 
 export default function Tabs(props) {
   let baseClass = "auth-tab";
+  const { tabList, activeTab, changeTab } = props
 
   return (
     <ul className="auth-tabs">
-      {props.tabList.map((tab) => {
+      {tabList.map((tab) => {
         return (
           <li key={tab.id}
-            className={tab.id === props.activeTab ? baseClass : `${baseClass} inactive-tab` }
-            onClick={props.changeTab.bind(this, tab)}
+            className={tab.id === activeTab ? baseClass : `${baseClass} inactive-tab` }
+            onClick={changeTab.bind(this, tab)}
           > {tab.text}
           </li>
         )
@@ -17,3 +19,14 @@ export default function Tabs(props) {
     </ul>
   )
 }
+
+Tabs.propTypes = {
+  tabList: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      text: PropTypes.string,
+    }),
+  ).isRequired,
+  activeTab: PropTypes.number.isRequired,
+  changeTab: PropTypes.func.isRequired,
+};
