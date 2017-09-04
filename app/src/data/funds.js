2681b1060468c705e03bcfd9eaca60122d82ccbb
Mateo Clarke <code@mateoclarke.com>
2017-09-04 13:15:02 -0500

    fix percent change inputs

--- app/src/data/funds.js
+++ app/src/data/funds.js
@@ -2,7 +2,7 @@ const funds = {
   generalFund: 974060000, // TOTAL FUNDS AVAILABLE
   generalFund2016: 911000000,
   generalFund2017: 974060000,
-  lastYearServicesSum: 969200000,
+  lastYearServicesSum: 974060000,
   sumOfServiceSpending: null,
   generalFundsRemaining: null,
 }
