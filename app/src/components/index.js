254f64acf2f2b18de7246049d3fe17972739190d
Mateo Clarke <code@mateoclarke.com>
2018-08-06 12:04:13 -0500

    test

--- app/src/components/index.js
+++ app/src/components/index.js
@@ -1,60 +1,59 @@
-import React, { Component } from 'react'
-import { Provider } from 'react-redux'
-import { IntlProvider, addLocaleData } from 'react-intl'
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { IntlProvider, addLocaleData } from 'react-intl';
 import en from 'react-intl/locale-data/en';
 import es from 'react-intl/locale-data/es';
-import { Route, Router, Redirect, Switch } from 'react-router-dom'
-import ReactGA from 'react-ga'
-import createHistory from 'history/createBrowserHistory'
+import { Route, Router, Redirect, Switch } from 'react-router-dom';
+import ReactGA from 'react-ga';
+import createHistory from 'history/createBrowserHistory';
 
-import Home from './Home'
-import Intro from './Intro'
-import DashboardContainer from '../containers/Dashboard'
-import ServiceContainer from '../containers/Service'
-import DepartmentContainer from '../containers/Department'
-import LearnMore from './Department/LearnMore'
-import ExplainContainer from '../containers/Explain'
-import SubmitContainer from '../containers/Submit'
-import User from './User'
-import Done from './Done'
-import Admin from './Admin/Index'
-import { firebaseAuth } from '../config/constants'
-import { logout } from '../helpers/auth'
+import Home from './Home';
+import Intro from './Intro';
+import DashboardContainer from '../containers/Dashboard';
+import ServiceContainer from '../containers/Service';
+import DepartmentContainer from '../containers/Department';
+import LearnMore from './Department/LearnMore';
+import ExplainContainer from '../containers/Explain';
+import SubmitContainer from '../containers/Submit';
+import User from './User';
+import Done from './Done';
+import Admin from './Admin/Index';
+import { firebaseAuth } from '../config/constants';
+import { logout } from '../helpers/auth';
 import Landing from './Landing';
 
 import store from '../store';
 
 // Google Analytics
-ReactGA.initialize('UA-64394324-4')
-const history = createHistory()
+ReactGA.initialize('UA-64394324-4');
+const history = createHistory();
 history.listen((location) => {
-  ReactGA.set({ page: location.pathname })
-  ReactGA.pageview(location.pathname)
+  ReactGA.set({ page: location.pathname });
+  ReactGA.pageview(location.pathname);
 });
 
 // Internationalization 🌎
 addLocaleData([...en, ...es]);
-const language = (navigator.languages && navigator.languages[0]) ||
-                     navigator.language ||
-                     navigator.userLanguage;
+const language =
+  (navigator.languages && navigator.languages[0]) || navigator.language || navigator.userLanguage;
 
 export default class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       authed: false,
       loading: true,
       user: {},
-    }
+    };
   }
 
   componentDidMount() {
-    this.removeListener = firebaseAuth().onAuthStateChanged(user => this.updateAuthState(user))
+    this.removeListener = firebaseAuth().onAuthStateChanged(user => this.updateAuthState(user));
   }
 
   componentWillUnmount() {
-    this.removeListener()
+    this.removeListener();
   }
 
   updateAuthState(user) {
@@ -63,26 +62,29 @@ export default class App extends Component {
         authed: true,
         loading: false,
         user,
-      })
+      });
     } else {
       this.setState({
         loading: false,
-      })
+      });
     }
   }
 
   handleLogout() {
-    const warning = confirm('Are you sure you want to log out?')
+    const warning = confirm('Are you sure you want to log out?');
     if (warning) {
-      console.log("LOGGED OUT")
-      logout()
-      this.setState({ authed: false })
+      console.log('LOGGED OUT');
+      logout();
+      this.setState({ authed: false });
     }
   }
 
   render() {
-    const { authed, user, loading } = this.state
-    return loading === true ? <h1>Loading</h1> : (
+    console.log('😈');
+    const { authed, user, loading } = this.state;
+    return loading === true ? (
+      <h1>Loading</h1>
+    ) : (
       <IntlProvider locale={language}>
         <Provider store={store}>
           <Router history={history}>
@@ -90,53 +92,82 @@ export default class App extends Component {
               <div className="row">
                 <Switch className="row">
                   <Route
-                    path="/" exact render={() => {
-                      return authed
-                      ? <Redirect to="/dashboard" />
-                      : <Redirect to="/home" />
+                    path="/"
+                    exact
+                    render={() => {
+                      return authed ? <Redirect to="/dashboard" /> : <Redirect to="/home" />;
                     }}
                   />
                   <Route path="/home">
                     <Landing />
                   </Route>
                   <Route
-                    path="/login" isAuthed={authed} render={() => {
-                      return authed
-                      ? <Redirect to="/intro/1" />
-                      : <Home />
+                    path="/login"
+                    isAuthed={authed}
+                    render={() => {
+                      return authed ? <Redirect to="/intro/1" /> : <Home />;
                     }}
                   />
                   <Route path="/intro/:id" render={props => <Intro {...props} />} />
-                  <Route path="/dashboard" render={props => <DashboardContainer {...props} user={user} />} />
-                  <Route path="/service/:id" exact render={props => <ServiceContainer {...props} />} />
-                  <Route path="/service/:service_id/department/:id" exact
+                  <Route
+                    path="/dashboard"
+                    render={props => <DashboardContainer {...props} user={user} />}
+                  />
+                  <Route
+                    path="/service/:id"
+                    exact
+                    render={props => <ServiceContainer {...props} />}
+                  />
+                  <Route
+                    path="/service/:service_id/department/:id"
+                    exact
                     render={props => <DepartmentContainer {...props} user={user} />}
                   />
-                  <Route path="/service/:service_id/department/:id/learn-more"
+                  <Route
+                    path="/service/:service_id/department/:id/learn-more"
                     render={props => <LearnMore {...props} />}
                   />
-                  <Route path="/service/:service_id/department/:id/explain"
+                  <Route
+                    path="/service/:service_id/department/:id/explain"
                     render={props => <ExplainContainer {...props} />}
                   />
-                  <Route path="/user" render={() => {
-                    return (
-                      <User
-                        isAuthed={authed}
-                        handleLogout={this.handleLogout.bind(this)}
-                        user={user}
-                      />
-                    )
-                  }} />
-                  <Route path="/submit" render={(props) => {
-                    return <SubmitContainer {...props} user={user} />
-                  }} />
-                  <Route path="/done" render={(props) => {
-                    return <Done {...props} />
-                  }} />
-                  <Route path="/judges" render={(props) => {
-                    return <Admin {...props} />
-                  }} />
-                  <Route render={() => <h3>404, you ain&apos;t supposed to be here. <a href="/">Go back.</a></h3>} />
+                  <Route
+                    path="/user"
+                    render={() => {
+                      return (
+                        <User
+                          isAuthed={authed}
+                          handleLogout={this.handleLogout.bind(this)}
+                          user={user}
+                        />
+                      );
+                    }}
+                  />
+                  <Route
+                    path="/submit"
+                    render={(props) => {
+                      return <SubmitContainer {...props} user={user} />;
+                    }}
+                  />
+                  <Route
+                    path="/done"
+                    render={(props) => {
+                      return <Done {...props} />;
+                    }}
+                  />
+                  <Route
+                    path="/judges"
+                    render={(props) => {
+                      return <Admin {...props} />;
+                    }}
+                  />
+                  <Route
+                    render={() => (
+                      <h3>
+                        404, you ain&apos;t supposed to be here. <a href="/">Go back.</a>
+                      </h3>
+                    )}
+                  />
                 </Switch>
               </div>
             </div>
