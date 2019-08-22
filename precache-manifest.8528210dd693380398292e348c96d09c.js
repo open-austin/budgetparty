a86b2980b9dd947a59a9e131467652471061346a
Mateo Clarke <code@mateoclarke.com>
2019-08-21 23:18:55 -0500

    Updates

---
+++ precache-manifest.8528210dd693380398292e348c96d09c.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0107797a69fd9de1fac4e823ee740bbe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa00b2806024e5a1287b",
+    "url": "/static/css/main.b8c6631b.chunk.css"
+  },
+  {
+    "revision": "d4cb7ae6cf719d188bab",
+    "url": "/static/js/2.cf48a452.chunk.js"
+  },
+  {
+    "revision": "aa00b2806024e5a1287b",
+    "url": "/static/js/main.c000b53a.chunk.js"
+  },
+  {
+    "revision": "27f1d23e942084403978",
+    "url": "/static/js/runtime~main.dd67ad75.js"
+  },
+  {
+    "revision": "be50f608aa1c08adac4da68ad775bcd8",
+    "url": "/static/media/0.be50f608.svg"
+  },
+  {
+    "revision": "f4bcb97a1ebbd34c23495814e60f1712",
+    "url": "/static/media/0.f4bcb97a.svg"
+  },
+  {
+    "revision": "06a13ecdf41bc6b76c6ba5688ac9e1ba",
+    "url": "/static/media/1.06a13ecd.svg"
+  },
+  {
+    "revision": "4509fddbc42398cc171a29f7d10233a4",
+    "url": "/static/media/1.4509fddb.svg"
+  },
+  {
+    "revision": "ed3a0d7545d265d2642b0e9f63aa0131",
+    "url": "/static/media/2.ed3a0d75.svg"
+  },
+  {
+    "revision": "fc54c1553ff96e68fec121a16fa48652",
+    "url": "/static/media/2.fc54c155.svg"
+  },
+  {
+    "revision": "ac54751cb1b173928d8dd555e773d7c2",
+    "url": "/static/media/3.ac54751c.svg"
+  },
+  {
+    "revision": "d57bf17ba2e384dc0d8883c03b9ffa83",
+    "url": "/static/media/3.d57bf17b.svg"
+  },
+  {
+    "revision": "3651c67a9c271ec8c5bfc4bcfb9d5f6b",
+    "url": "/static/media/4.3651c67a.svg"
+  },
+  {
+    "revision": "79509ce14aea9ada45b04f7746f7b07f",
+    "url": "/static/media/4.79509ce1.svg"
+  },
+  {
+    "revision": "70f0d9809aaa6716336abf33f5327cee",
+    "url": "/static/media/auth_service_google.70f0d980.svg"
+  },
+  {
+    "revision": "b6fb5f963319df0b2fc03c2c12e9154f",
+    "url": "/static/media/avatar.b6fb5f96.svg"
+  },
+  {
+    "revision": "04b0ee67eddd415916d7af2ef0cfe01b",
+    "url": "/static/media/back.04b0ee67.svg"
+  },
+  {
+    "revision": "499162a176b6843baf9af150da92b4c3",
+    "url": "/static/media/back_arrow.499162a1.svg"
+  },
+  {
+    "revision": "dce074a748f41e9b7bc00a12f5cd6e32",
+    "url": "/static/media/budget_party_logo.dce074a7.svg"
+  },
+  {
+    "revision": "a9a2c062f1f17eeda570563b60987cbb",
+    "url": "/static/media/check.a9a2c062.svg"
+  },
+  {
+    "revision": "3c05d8c1e2b26c3a10a7686a02c3baae",
+    "url": "/static/media/close.3c05d8c1.svg"
+  },
+  {
+    "revision": "9a9b67607dd1663f703faf80469d0fb7",
+    "url": "/static/media/comment.9a9b6760.svg"
+  },
+  {
+    "revision": "279ac2cb796705e0496e97916ccb9b5b",
+    "url": "/static/media/confetti.279ac2cb.svg"
+  },
+  {
+    "revision": "c179c84f613ef84dbf42f86d4b5e6dba",
+    "url": "/static/media/forward_arrow.c179c84f.svg"
+  },
+  {
+    "revision": "4ff771b74473593d7db592854f7e94f9",
+    "url": "/static/media/homepage-screens.4ff771b7.svg"
+  },
+  {
+    "revision": "710b4e4ea5fb0c3296f05ebdb8aba8f0",
+    "url": "/static/media/lock.710b4e4e.svg"
+  },
+  {
+    "revision": "416f450bf0a16092e748c3a07a9730b8",
+    "url": "/static/media/money.416f450b.svg"
+  },
+  {
+    "revision": "4fface59c5278cf75e6d84966df97cb7",
+    "url": "/static/media/pencil.4fface59.svg"
+  },
+  {
+    "revision": "ffe21c196b4bed66aafa1c47cea664b8",
+    "url": "/static/media/voice-icon.ffe21c19.svg"
+  }
+]);
